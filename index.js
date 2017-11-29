b318b0a806d73f3a9150fab4df7a91034198f9a6
William carter <bam1137@yahoo.com>
2017-11-29 23:08:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
 var height = 32
-var message = ` include ${name}`
+var message = ` include ${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
